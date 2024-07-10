1ba2fe388461039745e00c2ac3b214693909c32b
Gustavo Amaral Eliseu <30469845+GustavoEliseu@users.noreply.github.com>
2024-07-10 16:49:26 -0300

    rename screens to better notice the order

--- lib/screens/main_route/route/main_route.dart
+++ lib/screens/main_route/route/main_route.dart
@@ -5,11 +5,11 @@ import 'package:flutter_elo7/business/bloc/database_bloc/query_jobs_bloc.dart';
 import 'package:flutter_elo7/business/bloc/database_bloc/query_jobs_exists_bloc.dart';
 import 'package:flutter_elo7/business/bloc/dio_bloc/request_jobs_bloc.dart';
 import 'package:flutter_elo7/constants/ui_color.dart';
-import 'package:flutter_elo7/screens/main_route/open_jobs_screen.dart';
-import 'package:flutter_elo7/screens/main_route/our_team_screen.dart';
-import 'package:flutter_elo7/screens/main_route/results_screen.dart';
-import 'package:flutter_elo7/screens/main_route/seller_word_screen.dart';
-import 'package:flutter_elo7/screens/main_route/work_with_us_screen.dart';
+import 'package:flutter_elo7/screens/main_route/d_open_jobs_screen.dart';
+import 'package:flutter_elo7/screens/main_route/c_our_team_screen.dart';
+import 'package:flutter_elo7/screens/main_route/d_results_screen.dart';
+import 'package:flutter_elo7/screens/main_route/b_seller_word_screen.dart';
+import 'package:flutter_elo7/screens/main_route/a_work_with_us_screen.dart';
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
